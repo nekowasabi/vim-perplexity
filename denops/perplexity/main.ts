4b75c1786db562755d5fcf521b32a8be12ccf5ee
nekowasabi <nolifeking00@gmail.com>
2024-01-23 18:19:33 +0900

    fix(main): プロンプトの型をチェックする

--- denops/perplexity/main.ts
+++ denops/perplexity/main.ts
@@ -1,24 +1,29 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
 import * as v from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
+import { is } from "https://deno.land/x/unknownutil@v3.14.0/mod.ts";
 
 // main関数はプラグインを初期化し、ディスパッチャとコマンドを設定します
 export async function main(denops: Denops): Promise<void> {
   // プラグインの設定値を取得
   const TOKEN = await denops.eval("g:perplexity_token");
   const MODEL = await denops.eval("g:perplexity_model");
-  const LOG_DIRECTORY = await v.g.get(denops, "perplexity_log_directory") ?? undefined;
-  
+  const LOG_DIRECTORY = await v.g.get(denops, "perplexity_log_directory") ??
+    undefined;
+
   denops.dispatcher = {
     // chat関数は新しい分割を開き、チャットのバッファを設定します
     async chat() {
       // 新しいウィンドウを開き、チャット用のバッファを設定する
       await denops.cmd("new split");
       await denops.cmd("setlocal buftype=nofile");
+
       await denops.cmd("setlocal noswapfile");
       await denops.cmd("setlocal bufhidden=wipe");
       await denops.cmd("setlocal filetype=markdown");
 
+      const prompt = await denops.call("input", "Prompt > ");
+      await this.request(prompt);
 
       // for testing
       console.log(LOG_DIRECTORY);
@@ -28,6 +33,11 @@ export async function main(denops: Denops): Promise<void> {
       // ユーザーからのプロンプトを入力してもらう
       const prompt = await denops.call("input", "Prompt > ");
 
+      await this.request(prompt);
+    },
+
+    async request(prompt: unknown) {
+      is.String(prompt);
       // Perplexity APIにリクエストを送信し、結果を取得する
       const response = await fetch(
         "https://api.perplexity.ai/chat/completions",
